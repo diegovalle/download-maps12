707397133330fd768c2965505a4b69843b577d32
Diego Valle-Jones <diegovalle@gmail.com>
2013-02-15 21:26:57 -0600

    also unzip the distritos and secciones

--- unzip.sh
+++ unzip.sh
@@ -3,3 +3,13 @@ for i in zip/mexico/*.zip
 do      
   unzip $i -d unzip/mexico 
 done
+
+for i in zip/distrito/*.zip
+do      
+  unzip $i -d unzip/distrito 
+done
+
+for i in zip/seccion/*.zip
+do      
+  unzip $i -d unzip/seccion
+done
