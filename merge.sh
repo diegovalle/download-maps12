0876d1cd74df252c81f69cb826bb03f4627c5a43
Diego Valle-Jones <diegovalle@gmail.com>
2013-02-16 18:10:25 -0600

    correct directory for the secciones

--- merge.sh
+++ merge.sh
@@ -10,15 +10,15 @@ function merge {
   fi
 
   #Merge the files from Mexico's 32 states into one big shapefile
-  for file in unzip/distrito/*.shp
+  for file in unzip/$3/*.shp
   do
     echo ${file}
     ogr2ogr -update -append $1.shp ${file}  -nln $2 
   done
 }
 
-merge "map-out/distritos/mx_distrito" mx_distrito
+merge "map-out/distritos/mx_distrito" mx_distrito distrito
 #Since we want to add the INEGI municipality codes we merge it to a temporary file
-merge "unzip/seccion/mx_secciones_ife" mx_secciones_ife
+merge "unzip/seccion/mx_secciones_ife" mx_secciones_ife seccion
 
 
