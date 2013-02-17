d0e4cd3b8269ec734ba372c4af479caf1ba9c114
Diego Valle-Jones <diegovalle@gmail.com>
2013-02-16 18:11:18 -0600

    correct url for the distritos

--- download.sh
+++ download.sh
@@ -20,23 +20,29 @@ function download_inegi {
   if [ ! -f zip/mexico/$1 ];
     then
       $WGET $2 -O zip/mexico/$1
+      if [ $? -ne 0 ]
+        then
+          echo "##########################################################################"
+	  echo "Sometimes mapserver.inegi.org.mx goes down, try again after a few hours"
+          echo "##########################################################################"
+	  exit 1
+	fi
   fi
 }
 
+###Download Shapefiles from the INEGI
+#download_inegi municipios50.zip "http://mapserver.inegi.org.mx/data/mgm/redirect.cfm?fileX=MUNICIPIOS50"
 
-###Download Shapefiles from IFE
-download_ife distrito "http://gaia.inegi.org.mx/NLB/tunnel/IFE2010/Descarga.do?tabla=2&grupo=1,2,3,4,5,6,7,8,9,10,11,12&edo="
+#download_inegi estados50.zip "http://mapserver.inegi.org.mx/data/mgm/redirect.cfm?fileX=ESTADOS50"
+
+#download_inegi localidades-urbanas50.zip "http://mapserver.inegi.org.mx/data/mgm/redirect.cfm?fileX=LOCURBANAS50"
+
+#download_inegi localidades-rurales50.zip "http://mapserver.inegi.org.mx/data/mgm/redirect.cfm?fileX=LOCRURALES50"
+
+
+###Download Shapefiles from the IFE
+download_ife distrito "http://gaia.inegi.org.mx/NLB/tunnel/IFE2010/Descarga.do?tabla=1&grupo=1,2,3,4,5,6,7,8,9,10,11,12&edo="
 
 download_ife seccion "http://gaia.inegi.org.mx/NLB/tunnel/IFE2010/Descarga.do?tabla=2&grupo=1,2,3,4,5,6,7,8,9,10,11,12&edo="
 
-###Download Shapefiles from the INEGI
-echo "Sometimes mapserver.inegi.org.mx goes down, if so try again after a few hours"
-
-download_inegi municipios50.zip "http://mapserver.inegi.org.mx/data/mgm/redirect.cfm?fileX=MUNICIPIOS50"
-
-download_inegi estados50.zip "http://mapserver.inegi.org.mx/data/mgm/redirect.cfm?fileX=ESTADOS50"
-
-download_inegi localidades-urbanas50.zip "http://mapserver.inegi.org.mx/data/mgm/redirect.cfm?fileX=LOCURBANAS50"
-
-download_inegi localidades-rurales50.zip "http://mapserver.inegi.org.mx/data/mgm/redirect.cfm?fileX=LOCRURALES50"
 
